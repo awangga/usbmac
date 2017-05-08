4fc4eb3be1c6d9495fa899cbfbdd3718b38ef7c3
Rolly Maulana Awangga <rolly@awang.ga>
2017-05-08 09:28:14 +0700

    ok

--- usbmac.sh
+++ usbmac.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Untitled --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction &&say Done
+sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/usbmac --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction &&say Done
 
